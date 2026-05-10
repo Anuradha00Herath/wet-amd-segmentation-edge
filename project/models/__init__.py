a0618ea2e280d832caf2f9ebfae3fce90322b78b
Anuradha Herath <herathanuradha03@gmail.com>
2026-05-11 03:07:53 +0530

    fix the bug

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -1,8 +1,8 @@
 # models/__init__.py
-from models.baseline_model import BaselineModel, build_baseline
+from models.baseline_model import build_baseline, NUM_CLASSES, CLASS_NAMES
 from models.model_loader import get_model, load_model, SegmentationInference
 
 __all__ = [
-    "BaselineModel", "build_baseline",
+    "build_baseline", "NUM_CLASSES", "CLASS_NAMES",
     "get_model", "load_model", "SegmentationInference",
 ]
